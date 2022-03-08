5fcb458a96bb534cf13dab5330ebef6020b2cba7
Christopher Jenkins <45422356+chrisjen83@users.noreply.github.com>
2022-03-08 20:48:55 +1100

    Topic 2 upload

---
+++ Topic 2/Challenge 1.py
@@ -0,0 +1,34 @@
+# Program to test integer attributes. This program will test positive/negative and zero
+# as well as odd and even number.
+#----------------------------------------------------------------------------------------
+
+import sys
+
+# user enter number to test
+input_val = input('Enter a whole number to test: ')
+
+# Detect if number entered is integer, if a float or text is entered program will exit.
+try:
+    int(input_val)
+except ValueError:
+    print('Please enter a whole integer number, try again.')
+    sys.exit(1)
+
+#Convert the string vaule from input into an integer
+num = int(input_val)
+
+# Test for positive/negative or zero number
+if num > 0:
+    print(num, 'is a POSITIVE number')
+elif num == 0:
+    print(num, 'is Zero')
+else:
+    print(num, ' is a NEGATIVE number')
+
+# Test to determine if number is odd or even
+if num % 2 == 0:
+    print(num, 'is an even number.')
+else:
+    print(num, 'is an odd number.')
+
+sys.exit(0)
---
+++ Topic 2/Challenge 2.py
@@ -0,0 +1,37 @@
+# Program to calculate the cost of word processing based on a fixed rate card
+
+# Declare constant variables
+import sys
+
+WORD_PER_MIN = 30
+CHARGE_8_LESS = 21.75
+CHARGE_8_PLUS = 25
+HOUR_FACTOR = 60
+
+# Ask user to provide number of words in a document
+input_val = input('Please enter the number of words to type: ')
+
+# Detect if number entered is a positive integer. Word count cannot be a float or negative numbers.
+if not input_val.isnumeric():
+    print('Please enter a whole positive integer number, try again.')
+    sys.exit(1)
+
+#Convert the string vaule from input into an integer
+word_count = int(input_val)
+
+#Convert words per 30 min to words per hour in int using Python Floor
+work_hours = (word_count // WORD_PER_MIN)//HOUR_FACTOR
+print(f'{word_count:,} words will take {work_hours:,}Hrs of labour.')
+
+# Evaluate cost to customer for declared word count. Since rate card is per hour for word counts less than 1hr
+# of labour a flat rate of $21.75 will be charged.
+if work_hours >= 1 and work_hours <= 8:
+    customer_cost = work_hours * CHARGE_8_LESS
+    print(f'For a document containing {word_count:,} word will cost ${customer_cost:,.2f}')
+elif work_hours == 0 and word_count <= 1800:
+    print(f'For a document containing {word_count:,} word will cost a flat rate of ${CHARGE_8_LESS:,.2f}')
+else:
+    customer_cost = work_hours * CHARGE_8_PLUS
+    print(f'For a document containing {word_count:,} word will cost ${customer_cost:,.2f}')
+
+sys.exit(0)
